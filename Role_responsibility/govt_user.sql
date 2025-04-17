6d569eb4b7fe4bbb58cc52f742464a4e3760c5d2
Akassh Paramasivan <paramasivan.a@northeastern.edu>
2025-04-16 22:57:05 -0400

    role bug fixes

--- Role_responsibility/govt_user.sql
+++ Role_responsibility/govt_user.sql
@@ -4,9 +4,17 @@
 -- WHERE food_id = 2;
 
 UPDATE food_admin.govt_food_view
-   SET quality = 9,
+   SET quality = 8,
        user_id_gov = 2
  WHERE food_id = 1;
+ UPDATE food_admin.govt_food_view
+   SET quality = 3,
+       user_id_gov = 2
+ WHERE food_id = 5;
+ UPDATE food_admin.govt_food_view
+   SET quality = 6,
+       user_id_gov = 2
+ WHERE food_id = 4;
  
 COMMIT;
 
--- Role_responsibility/supplier_user.sql
+++ Role_responsibility/supplier_user.sql
@@ -5,9 +5,29 @@ VALUES (
   'Supplier One Inc.',
   (SELECT user_id FROM food_Admin.user_details WHERE user_name = 'supplier_user')
 );
+INSERT INTO food_Admin.supplier (supplier_id, supplier_name, user_id_supplier)
+VALUES (
+  food_Admin.supplier_seq.NEXTVAL,
+  'Supplier two Inc.',
+  (SELECT user_id FROM food_Admin.user_details WHERE user_name = 'supplier_user2')
+);
+INSERT INTO food_Admin.supplier (supplier_id, supplier_name, user_id_supplier)
+VALUES (
+  food_Admin.supplier_seq.NEXTVAL,
+  'Supplier three Inc.',
+  (SELECT user_id FROM food_Admin.user_details WHERE user_name = 'supplier_user3')
+);
 
 INSERT INTO food_Admin.v_food_supplier (food_name, unit_of_measure, total_quantity, supplier_id)
-VALUES ('Apples', 'KG', 100, 1);
+VALUES ('Apples', 'KG', 15, 1);
+INSERT INTO food_Admin.v_food_supplier (food_name, unit_of_measure, total_quantity, supplier_id)
+VALUES ('Pizza', 'KG', 45, 2);
+INSERT INTO food_Admin.v_food_supplier (food_name, unit_of_measure, total_quantity, supplier_id)
+VALUES ('Brownie', 'KG', 5, 3);
+INSERT INTO food_Admin.v_food_supplier (food_name, unit_of_measure, total_quantity, supplier_id)
+VALUES ('Momos', 'KG', 25, 2);
+INSERT INTO food_Admin.v_food_supplier (food_name, unit_of_measure, total_quantity, supplier_id)
+VALUES ('Fries', 'KG', 60, 3);
 COMMIT;
 show user
 
